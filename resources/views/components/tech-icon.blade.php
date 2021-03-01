68fef147f07ccc925e610d95cdc1965a73a017a2
Julio Fajardo <jfajardoh5@gmail.com>
2021-03-01 11:39:25 -0500

    Refactored index view to use more blade components. It looks cleaner now.

---
+++ resources/views/components/tech-icon.blade.php
@@ -0,0 +1,6 @@
+<div class="flex flex-col justify-center items-center text-center">
+    <svg viewBox="0 0 128 128" class="w-12 md:w-20">
+        {{ $slot }}
+    </svg>
+    <span class="mt-1">{{ $label }}</span>
+</div>
