2331d1fbaebee9d82190c186e5722b70af23cd4a
Julio Fajardo <jfajardoh5@gmail.com>
2021-03-03 23:57:25 -0500

    Enabled telescope night mode, enabled telescope regardless of environment.

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -16,14 +16,15 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
      */
     public function register()
     {
-        // Telescope::night();
+        Telescope::night();
 
         $this->hideSensitiveRequestDetails();
 
         Telescope::filter(function (IncomingEntry $entry) {
-            if ($this->app->environment('local')) {
-                return true;
-            }
+            // if ($this->app->environment('local')) {
+            //     return true;
+            // }
+            return true;
 
             return $entry->isReportableException() ||
                    $entry->isFailedRequest() ||
